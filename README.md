6b3673a728ebe2cf12c8369030a2f51f72791d95
Patrick Molka <molka.patrick@gmail.com>
2024-09-12 15:52:54 -0400

    added projects

---
+++ README.md
@@ -0,0 +1,17 @@
+# My Portfolio Page --- This is Still A WIP
+To be hosted on netifly
+
+Future Plans:
+gsap integration for scroll lock and more interactive webpages
+Better css color choices
+Project demos ==> add UI for interactive projects
+
+
+
+
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+
+Currently, two official plugins are available:
+
+- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
+- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Patrick Molka</h1>
+        <p className={styles.description}>
+          I'm a Software Engineering student with an interest in exciting projects!
+        </p>
+        <a href="mailto:molka.patrick@gmail.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/heroImage.jpg")}
+        alt="image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
